107a7c687d4082498ae53df587e6cf5f874ee8c7
Ian Tewksbury <itewk@redhat.com>
2018-04-10 08:15:15 -0400

    release_ip - update to work with vm retirment process and add some basic error checking

--- Automate/RedHatConsulting_Infoblox/Integration/Infoblox/Operations/Methods.class/__methods__/release_ip.rb
+++ Automate/RedHatConsulting_Infoblox/Integration/Infoblox/Operations/Methods.class/__methods__/release_ip.rb
@@ -10,6 +10,8 @@ require 'rest-client'
 require 'json'
 require 'base64'
 
+INFOBLOX_CONFIG = $evm.instantiate('Integration/Infoblox/Configuration/default')
+
 # Log an error and exit.
 #
 # @param msg Message to error with
@@ -20,7 +22,49 @@ def error(msg)
   exit MIQ_STOP
 end
 
-INFOBLOX_CONFIG = $evm.instantiate('Integration/Infoblox/Configuration/default')
+# Notify and log a warning message.
+#
+# @param msg Message to warn with
+def warn(msg)
+  $evm.create_notification(:level => 'warning', :message => msg)
+  $evm.log(:warn, msg)
+end
+
+# There are many ways to attempt to pass parameters in Automate.
+# This function checks all of them in priorty order as well as checking for symbol or string.
+#
+# Order:
+#   1. Inputs
+#   2. Current
+#   3. Object
+#   4. Root
+#   5. State
+#
+# @return Value for the given parameter or nil if none is found
+def get_param(param)  
+  # check if inputs has been set for given param
+  param_value ||= $evm.inputs[param.to_sym]
+  param_value ||= $evm.inputs[param.to_s]
+  
+  # else check if current has been set for given param
+  param_value ||= $evm.current[param.to_sym]
+  param_value ||= $evm.current[param.to_s]
+ 
+  # else cehck if current has been set for given param
+  param_value ||= $evm.object[param.to_sym]
+  param_value ||= $evm.object[param.to_s]
+  
+  # else check if param on root has been set for given param
+  param_value ||= $evm.root[param.to_sym]
+  param_value ||= $evm.root[param.to_s]
+  
+  # check if state has been set for given param
+  param_value ||= $evm.get_state_var(param.to_sym)
+  param_value ||= $evm.get_state_var(param.to_s)
+
+  $evm.log(:info, "{ '#{param}' => '#{param_value}' }") if @DEBUG
+  return param_value
+end
 
 def infoblox_request(action, path, payload=nil)
   #https://infoblox_server/wapidoc/ for reference
@@ -44,22 +88,66 @@ def infoblox_request(action, path, payload=nil)
 end
 
 begin
-  # Get provisioning object
-  prov = $evm.root['miq_provision']
-  error('Provisioning request not found') if prov.nil?
-  # get the VM
-  vm = prov.vm
-  error('VM on provisioning request not found') if vm.nil?
-  name = vm.name
-
-  $evm.log(:info, "vm = #{vm}") if @DEBUG
+  $evm.log(:info, "$evm.root['vmdb_object_type'] => '#{$evm.root['vmdb_object_type']}'") if @DEBUG
+  case $evm.root['vmdb_object_type']
+    when 'miq_provision'
+      $evm.log(:info, "Get VM and dialog attributes from $evm.root['miq_provision']") if @DEBUG
+      miq_provision = $evm.root['miq_provision']
+      vm            = miq_provision.vm
+      options       = miq_provision.options
+      
+      #merge the ws_values, dialog, top level options into one list to make it easier to search
+      options = options.merge(options[:ws_values]) if options[:ws_values]
+      options = options.merge(options[:dialog])    if options[:dialog]
+    when 'vm'
+      $evm.log(:info, "Get VM from paramater and dialog attributes form $evm.root") if @DEBUG
+      vm      = get_param(:vm)
+      options = $evm.root.attributes
+      
+      #merge the ws_values, dialog, top level options into one list to make it easier to search
+      options = options.merge(options[:ws_values]) if options[:ws_values]
+      options = options.merge(options[:dialog])    if options[:dialog]
+    when 'automation_task'
+      $evm.log(:info, "Get VM from paramater and dialog attributes form $evm.root") if @DEBUG
+      automation_task = $evm.root['automation_task']
+      dump_object("automation_task", automation_task) if @DEBUG
+      
+      vm  = get_param(:vm)
+  end
+  error('VM parameter not found') if vm.nil?
+  $evm.log(:info, "vm => #{vm}") if @DEBUG
 
   if INFOBLOX_CONFIG.nil? or INFOBLOX_CONFIG['server'] == 'infoblox.example.com'
     error("Infoblox configuration must be defined")
   else
-    host = infoblox_request(:get, "record:host?name=#{name}").first["_ref"]
-    infoblox_request(:delete, host)
+    # determine vm hostname, first try to get hostname entry, else use vm name
+    vm_hostname   = vm.hostnames.first if !vm.hostnames.empty?
+    vm_hostname ||= vm.name
+    
+    # get the infoblox host record to delete
+    $evm.log(:info, "Delete Infoblox record for hostname <#{vm_hostname}>")
+    infoblox_host_records = infoblox_request(:get, "record:host?name=#{vm_hostname}")
+    
+    # if Infoblox host records found to delete then delete them
+    # else warn that no host records found to delete and move on.
+    if !infoblox_host_records.empty?
+      infoblox_host_records.each do |infoblox_host_record|
+        infoblox_host_record_ref = infoblox_host_record['_ref']
+        $evm.log(:info, "Delete Infroblox host record <#{infoblox_host_record_ref}> for hostname <#{vm_hostname}>") if @DEBUG
+        
+        # delete the infoblox host record
+        begin
+          infoblox_request(:delete, infoblox_host_record_ref)
+          $evm.log(:info, "Deleted Infoblox record <#{infoblox_host_record_ref}> for hostname <#{vm_hostname}>")
+        rescue => delete_err
+          warn("Error deleting Infoblox host record for hostname <#{vm_hostname}>. Ignoring & Skipping. #{delete_err}")
+        end
+      end
+    else
+      warn("No Infoblox host record to delete found for hostname <#{vm_hostname}>. Ignoring & Skipping.")
+    end
   end
-rescue
-  error("Error releasing IP address")
+rescue => err
+  $evm.log(:error, "[#{err}]\n#{err.backtrace.join("\n")}")
+  error("Error deleting Infoblox DNS entry: #{err}")
 end
