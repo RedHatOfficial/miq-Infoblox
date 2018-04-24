a3bda328d54b5beb086e9d49869aa5d2dd276c33
Ian Tewksbury <itewk@redhat.com>
2018-04-24 13:53:33 -0400

    Update release and aquire IP address operations to work with new miq-Utiltites generic IP address operations. Remove dependency on network tags and instead use network configuration instances.

--- Automate/RedHatConsulting_Infoblox/Integration/Infoblox/Operations/Methods.class/__methods__/acquire_ip_address.rb
+++ Automate/RedHatConsulting_Infoblox/Integration/Infoblox/Operations/Methods.class/__methods__/acquire_ip_address.rb
@@ -1,8 +1,9 @@
-# Creates a host record in Infoblox for the given VM associated with a provisioning request
+# Creates a host record in Infoblox for the given VM and network configuration
 #
-# EXPECTED
-#   EVM ROOT
-#     miq_provision - VM Provisioning request to create the Infoblox host record for.
+# @param network_name String Name of the network to acquire an IP for
+#
+# @sets acquired_ip_address String IP address acquired from DDI provider
+#
 @DEBUG = false
 
 require 'rest-client'
@@ -38,6 +39,14 @@ def dump_root
   $evm.log("info", "Listing Root Object Attributes:") 
   $evm.root.attributes.sort.each { |k, v| $evm.log("info", "\t#{k}: #{v}") }
   $evm.log("info", "===========================================") 
+end
+
+# Notify and log a warning message.
+#
+# @param msg Message to warn with
+def warn(msg)
+  $evm.create_notification(:level => 'warning', :message => msg)
+  $evm.log(:warn, msg)
 end
 
 # Function for getting the current VM and associated options based on the vmdb_object_type.
@@ -140,6 +149,30 @@ def get_param(param)
   return param_value
 end
 
+# Get the network configuration for a given network
+#
+# @param network_name Name of the network to get the configuraiton for
+# @return Hash Configuration information about the given network
+#                network_purpose
+#                network_address_space
+#                network_gateway
+#                network_nameservers
+#                network_ddi_provider
+@network_configurations         = {}
+@missing_network_configurations = {}
+NETWORK_CONFIGURATION_URI       = 'Infrastructure/Network/Configuration'.freeze
+def get_network_configuration(network_name)
+  if @network_configurations[network_name].blank? && @missing_network_configurations[network_name].blank?
+    begin
+      @network_configurations[network_name] = $evm.instantiate("#{NETWORK_CONFIGURATION_URI}/#{network_name}")
+    rescue => e
+      @missing_network_configurations[network_name] = "WARN: No network configuration exists"
+      $evm.log(:warn, "No network configuration for Network <#{network_name}> exists")
+    end
+  end
+  return @network_configurations[network_name]
+end
+
 def infoblox_request(action, path, payload=nil)
   #https://infoblox_server/wapidoc/ for reference
   #https://community.infoblox.com/t5/API-Integration/The-definitive-list-of-REST-examples/td-p/1214
@@ -152,35 +185,28 @@ def infoblox_request(action, path, payload=nil)
   url = url_base+path
 
   params = {
-    :method=>action, :url=>url_base+path, :verify_ssl=>false,
-    :headers=>{ :content_type=>:json, :accept=>:json,
-                :authorization=>"Basic #{Base64.encode64("#{infoblox_username}:#{infoblox_password}")}"}
+    :method     => action,
+    :url        => url_base+path,
+    :verify_ssl => false,
+    :headers    => {
+      :content_type  => :json,
+      :accept        => :json,
+      :authorization => "Basic #{Base64.encode64("#{infoblox_username}:#{infoblox_password}")}"
+    }
   }
   params[:payload] = payload if payload
-  response = RestClient::Request.new(params).execute
-  return JSON.parse(response)
-end
-
-# IMPLIMENTORS: Modify as necessary
-#
-# Use this method to pick the subnet on which to create Infoblox records
-# Should return a string subnet in the format IP/netmask e.g. "127.31.0.0/24"
-def get_network_address_space(options)
-  network_name                   = options[:destination_network] || options['destination_network'] || get_param(:dialog_destination_network)
-  $evm.log(:info, "network_name                   => #{network_name}")                   if @DEBUG
-  network                        = $evm.vmdb(:lan).find_by_name(network_name)
-  $evm.log(:info, "network                        => #{network}")                        if @DEBUG
-  network_address_space_tag_name = network.tags(ADDRESS_SPACE_TAG_CATEGORY).first
-  $evm.log(:info, "network_address_space_tag_name => #{network_address_space_tag_name}") if @DEBUG
-  network_address_space_tag      = $evm.vmdb(:classification).find_by_name("#{ADDRESS_SPACE_TAG_CATEGORY}/#{network_address_space_tag_name}")
-  $evm.log(:info, "network_address_space_tag      => #{network_address_space_tag}")      if @DEBUG
-  network_address_space          = network_address_space_tag.description
-  $evm.log(:info, "network_address_space          => #{network_address_space}")          if @DEBUG
   
-  return network_address_space
+  $evm.log(:info, "Infoblox request params: #{params}")   if @DEBUG
+  $evm.log(:info, "Infoblox request payload: #{payload}") if @DEBUG
+
+  begin
+    response = RestClient::Request.new(params).execute
+    return JSON.parse(response)
+  rescue => e
+    error("Error making Infoblox request: #{e.response}")
+  end
 end
 
-# IMPLIMENTORS: DO NOT MODIFY
 begin
   dump_root()    if @DEBUG
   dump_current() if @DEBUG
@@ -191,12 +217,23 @@ begin
   if INFOBLOX_CONFIG.nil? or INFOBLOX_CONFIG['server'] == 'infoblox.example.com'
     error("Infoblox configuration must be defined")
   else
+    # get the network configuration
+    network_name = options[:network_name] || options[:dialog_network_name] || get_param(:network_name) || get_param(:dialog_network_name)
+    $evm.log(:info, "network_name => #{network_name}") if @DEBUG
+    network_configuration = get_network_configuration(network_name)
+    $evm.log(:info, "network_configuration => #{network_configuration}") if @DEBUG
+    
     # get the network_address_space
-    network_address_space = get_network_address_space(options)
+    network_address_space = network_configuration['network_address_space']
+    $evm.log(:info, "network_address_space => #{network_address_space}") if @DEBUG
 
+    # determine vm hostname, first try to get hostname entry, else use vm name
+    vm_hostname   = vm.hostnames.first if !vm.hostnames.empty?
+    vm_hostname ||= vm.name
+    
     # create the new DNS record
     payload = {
-      "name"              => vm.name,
+      "name"              => vm_hostname,
       "ipv4addrs"         => [{"ipv4addr"=>"func:nextavailableip:#{network_address_space}"}],
       "configure_for_dns" => true
     }
@@ -209,10 +246,10 @@ begin
     error("Failed to get IP from Infoblox") if ip.nil?
     $evm.log(:info, "New IP address from Infoblox: "+ip) if @DEBUG
     
-    # save the destination IP for use later
-    $evm.object['destination_ip'] = ip
-    $evm.set_state_var(:destination_ip, ip)
-    $evm.log(:info, "$evm.object['destination_ip'] => #{$evm.object['destination_ip']}") if @DEBUG
+    # save the acquired IP for use later
+    $evm.object['acquired_ip_address'] = ip
+    $evm.set_state_var(:acquired_ip_address, ip)
+    $evm.log(:info, "$evm.object['acquired_ip_address'] => #{$evm.object['acquired_ip_address']}") if @DEBUG
   end
 rescue => err
   $evm.log(:error, "[#{err}]\n#{err.backtrace.join("\n")}")
